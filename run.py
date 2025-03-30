67e897d9c7b49e3a9a0a1c7aa568dc3eb2572cc4
León555555 <lvidelaramos@gmail.com>
2025-03-30 15:16:59 +0200

    Crear tablas automáticamente al iniciar

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+from app.extensions import db
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()  # Esto crea las tablas si no existen
+
+if __name__ == "__main__":
+    app.run()

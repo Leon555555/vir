ac42addfb1aa15e99eca6c579f57e9b1723588ac
León555555 <lvidelaramos@gmail.com>
2025-04-01 11:04:58 +0200

    Crear tablas automáticamente en la base de datos

--- run.py
+++ run.py
@@ -4,3 +4,14 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run()
+from app import create_app
+from app.extensions import db
+
+app = create_app()
+
+# CREA LAS TABLAS si no existen (usado solo una vez)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run()

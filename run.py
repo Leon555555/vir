c94d2c8b12aa8532b836f2cde24e43b65d20e2ad
León555555 <lvidelaramos@gmail.com>
2025-04-21 17:38:30 +0200

    Corrige run.py para evitar migraciones automáticas en producción

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+# run.py
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    from flask_migrate import upgrade
+    with app.app_context():
+        upgrade()  # Solo se ejecuta si corres localmente: python run.py
+    app.run(debug=True)

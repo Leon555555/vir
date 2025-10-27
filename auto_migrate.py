1438b8f4fcab1a4613ba87c4dc3e5f2b8faf2a6f
León555555 <lvidelaramos@gmail.com>
2025-10-27 16:30:36 +0100

    Versión inicial de Vir

--- auto_migrate.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.extensions import db
-from flask_migrate import upgrade
-
-app = create_app()
-
-with app.app_context():
-    upgrade()
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "==> Instalando dependencias..."
-pip install -r requirements.txt
-
-echo "==> Aplicando migraciones..."
-flask db upgrade
-
